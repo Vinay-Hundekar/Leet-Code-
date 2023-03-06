5e0173e5865d09e292150926d710f9c05db27401
Vinay P Hundekar <vinayphundekar@gmail.com>
2023-03-06 18:33:50 +0530

    182. Duplicate Emails

---
+++ Leet SQL/182. Duplicate Emails.sql
@@ -0,0 +1,19 @@
+/*
+Table: Person
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+Write an SQL query to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+*/
+SELECT email
+FROM Person
+GROUP BY email 
+HAVING COUNT(email)>1
+;
