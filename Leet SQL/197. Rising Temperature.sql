10fce11ecab211801dfdf83bdc79c13a2a54ddf1
Vinay P Hundekar <vinayphundekar@gmail.com>
2023-03-10 17:48:59 +0530

    197. Rising Temperature

---
+++ Leet SQL/197. Rising Temperature.sql
@@ -0,0 +1,23 @@
+/*
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature on a certain day.
+ 
+
+Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+*/
+# Write your MySQL query statement below
+SELECT w1.id
+FROM Weather w1 
+JOIN Weather w2 ON
+DATEDIFF(w1.recordDate,w2.recordDate)=1 
+AND w1.temperature > w2.temperature
+;
