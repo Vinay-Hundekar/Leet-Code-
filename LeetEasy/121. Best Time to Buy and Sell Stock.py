65eac876be6473c9a4181e0550a8f3c55798e454
Vinay P Hundekar <vinayphundekar@gmail.com>
2023-03-29 23:31:32 +0530

    adding comments to 121

--- LeetEasy/121. Best Time to Buy and Sell Stock.py
+++ LeetEasy/121. Best Time to Buy and Sell Stock.py
@@ -4,11 +4,17 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        # Initializing profit to zero and minimum price to first price
         profit=0
         minp=prices[0]
         for i in range(len(prices)-1):
+            # Checking if price is lesser than minimumprice
+            # If so update minimum price
             if prices[i] < minp:
                 minp=prices[i]
+            # Checking if current prise - minimum price is greater then profit
+            # If so update profit
             if (prices[i+1]-minp)>profit:
                 profit=prices[i+1]-minp
+        # Return profit
         return profit
