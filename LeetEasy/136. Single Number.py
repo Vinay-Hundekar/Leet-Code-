615a8efa130237b1537c334feadf340e87d51d7d
Vinay P Hundekar <vinayphundekar@gmail.com>
2023-04-08 23:49:01 +0530

    Updated 136

--- LeetEasy/136. Single Number.py
+++ LeetEasy/136. Single Number.py
@@ -4,8 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # Initialy result will be zero
         result = 0
         for num in nums:
+            # We take exor or each number
+            # Because exor a number gives
+            # Sum if a number is exored with a number lesser than it
+            # Absolute difference if a number is exored with a number greater than it
+            # Zero if exored with same number
+            # Hence when a list number which has all the element repeted twice except one
+            # is exored then it will give us the element which is distinct in the list
             result ^= num        
+        # Return the obtained value
         return result
             
